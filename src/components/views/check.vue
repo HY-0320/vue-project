182926c6792ec6a59d161d0526bbf7b7f9d8da40
PIG-8 <815673746@qq.com>
2019-11-16 19:31:49 +0800

    pig

--- src/components/views/check.vue
+++ src/components/views/check.vue
@@ -1,11 +1,42 @@
 <template>
   <div id="myid">
     <div>
-      <router-link to="/"><h1 style="color:white" >HOME</h1></router-link>
+      <router-link to="/">
+        <h1 style="color:white">HOME</h1>
+      </router-link>
       <br />
+      <div id="myTable">
+        <input type="button" value="姓名:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].name" class="text"/>
+        <br>
+        <br>
+        <input type="button" value="班级:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].class" class="text" />
+        <br>
+        <br>
+        <input type="button" value="学号:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].studentId" class="text">
+        <br>
+        <br>
+        <input type="button" value="联系电话:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].phone" class="text" >
+        <br>
+        <br>
+        <input type="button" value="个人邮箱:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].e_mail" class="text">
+        <br>
+        <br>
+        <input type="button" value="QQ:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].qq" class="text">
+      </div>
     </div>
 
-    <h1>{{gender}}</h1>
   </div>
 </template>
 
@@ -14,7 +45,7 @@ import home from "./home.vue";
 export default {
   data() {
     return {
-      gender: "she is a girl!"
+
     };
   }
 };
@@ -22,11 +53,25 @@ export default {
 
 
 <style scoped>
-#myid{
-  background: url('../../assets/stuBackground.png');
+#myid {
+  background: url("../../assets/stuBackground.png");
   position: absolute;
   width: 100%;
   height: 100%;
 }
-
+#myTable{
+  position:fixed;
+	top:120px;
+	right:700px;
+}
+.button{
+  width: 100px;
+  font-size: 16px;
+  background-color: darkturquoise
+}
+.text{
+  width: 200px;
+  font-size: 21px;
+  background-color: aquamarine;
+}
 </style>
--- src/components/views/edit.vue
+++ src/components/views/edit.vue
@@ -4,7 +4,37 @@
       <router-link to="/"><h1 style="color:white" >HOME</h1></router-link>
       <br />
     </div>
-    <h1>{{gender}}</h1>
+    <div id="myTable">
+        <input type="button" value="姓名:" class="button">
+        <br>
+        <input type="text"  :value="this.$store.state.tableData[0].name"  class="text"/>
+        <br>
+        <br>
+        <input type="button" value="班级:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].class" class="text" />
+        <br>
+        <br>
+        <input type="button" value="学号:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].studentId" class="text">
+        <br>
+        <br>
+        <input type="button" value="联系电话:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].phone" class="text" >
+        <br>
+        <br>
+        <input type="button" value="个人邮箱:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].e_mail" class="text">
+        <br>
+        <br>
+        <input type="button" value="QQ:" class="button">
+        <br>
+        <input type="text" :value="this.$store.state.tableData[0].qq" class="text">
+    </div>
+
   </div>
 </template>
 
@@ -14,7 +44,7 @@ export default {
   
   data() {
     return {
-      gender: "Can be used to edit some information about students!"
+
     };
   }
 };
@@ -27,5 +57,20 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
+}
+#myTable{
+  position:fixed;
+	top:120px;
+	right:700px;
+}
+.button{
+  width: 100px;
+  font-size: 16px;
+  background-color:gold;
+}
+.text{
+  width: 200px;
+  font-size: 21px;
+  background-color:lightcoral;
 }
 </style>
