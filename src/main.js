aec92663478f4635f826934e289d763b78511f6c
PIG-8 <815673746@qq.com>
2019-11-16 19:33:52 +0800

    pig

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+
+Vue.prototype.$axios = axios 
+import axios from 'axios'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  ElementUI,
+  store,
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
+
